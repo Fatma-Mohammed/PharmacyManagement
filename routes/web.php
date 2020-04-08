02943a3df20ce92dda603e00bfcb42626dfd5f84
nouran <nouranebrahim9@gmail.com>
2020-04-08 16:21:01 +0200

    crud operations in orders table

--- routes/web.php
+++ routes/web.php
@@ -21,3 +21,10 @@ Auth::routes(['verify' => true]);
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/orders', 'OrderController@index')->name('orders.index');
+Route::get('/orders/create','OrderController@create')->name('orders.create');
+Route::post('/orders','OrderController@store')->name('orders.store');
+Route::get('/orders/{order}','OrderController@show')->name('orders.show');
+Route::get('/orders/{order}/destroy','OrderController@destroy')->name('orders.destroy');
+Route::get('/orders/{order}/edit','OrderController@edit')->name('orders.edit');
+Route::get('/orders/{order}/update','OrderController@update')->name('orders.update');

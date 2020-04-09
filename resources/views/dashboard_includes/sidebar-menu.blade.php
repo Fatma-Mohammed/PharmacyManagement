efdd6b2a66287bda396cfb2a8bbd3d5dd38cd4eb
Emad Saeed <e@emads3.com>
2020-04-09 12:04:39 +0200

    edit initial sidebar links

--- resources/views/dashboard_includes/sidebar-menu.blade.php
+++ resources/views/dashboard_includes/sidebar-menu.blade.php
@@ -1,7 +1,10 @@
 <div class="col-md-3 left_col">
     <div class="left_col scroll-view">
         <div class="navbar nav_title" style="border: 0;">
-            <a href="/" class="site_title"><i class="fa fa-filter"></i> <span>Pharmacy.</span></a>
+            <a href="/" class="site_title">
+                <i class="fa fa-filter"></i>
+                <span>{{ config('app.name', 'Pharmacy Manager') }}</span>
+            </a>
         </div>
 
         <div class="clearfix"></div>
@@ -25,6 +28,7 @@
             <div class="menu_section">
                 <h3>General</h3>
                 <ul class="nav side-menu">
+
                     <li><a><i class="fa fa-home"></i> Home <span class="fa fa-chevron-down"></span></a>
                         <ul class="nav child_menu">
                             <li><a href="/">Dashboard</a></li>
@@ -32,56 +36,72 @@
                             <li><a href="#">Dashboard3</a></li>
                         </ul>
                     </li>
-                    <li><a><i class="fa fa-edit"></i> Menu 1 <span class="fa fa-chevron-down"></span></a>
+
+                    <li><a><i class="fa fa-edit"></i> Pharmacies <span class="fa fa-chevron-down"></span></a>
                         <ul class="nav child_menu">
                             <li><a href="#">Sub Menu 1</a></li>
                             <li><a href="#">Sub Menu 2</a></li>
                         </ul>
                     </li>
-                    <li><a><i class="fa fa-desktop"></i> UI Elements <span
+
+                    <li><a><i class="fa fa-desktop"></i> Doctors <span
                                 class="fa fa-chevron-down"></span></a>
                         <ul class="nav child_menu">
                             <li><a href="#">Sub Menu 1</a></li>
                             <li><a href="#">Sub Menu 2</a></li>
                         </ul>
                     </li>
-                    <li><a><i class="fa fa-table"></i> Tables <span class="fa fa-chevron-down"></span></a>
+
+                    <li><a><i class="fa fa-table"></i> Users <span class="fa fa-chevron-down"></span></a>
                         <ul class="nav child_menu">
                             <li><a href="#">Sub Menu 1</a></li>
                             <li><a href="#">Sub Menu 2</a></li>
                         </ul>
                     </li>
-                    <li><a><i class="fa fa-bar-chart-o"></i> Data Presentation <span
+
+                    <li><a><i class="fa fa-bar-chart-o"></i> Areas <span
                                 class="fa fa-chevron-down"></span></a>
                         <ul class="nav child_menu">
                             <li><a href="#">Sub Menu 1</a></li>
                             <li><a href="#">Sub Menu 2</a></li>
                         </ul>
                     </li>
-                    <li><a><i class="fa fa-clone"></i>Layouts <span class="fa fa-chevron-down"></span></a>
+
+                    <li><a><i class="fa fa-clone"></i> User Addresses <span class="fa fa-chevron-down"></span></a>
                         <ul class="nav child_menu">
                             <li><a href="#">Sub Menu 1</a></li>
                             <li><a href="#">Sub Menu 2</a></li>
                         </ul>
                     </li>
+
+                    <li><a><i class="fa fa-clone"></i> Medicines <span class="fa fa-chevron-down"></span></a>
+                        <ul class="nav child_menu">
+                            <li><a href="#">Sub Menu 1</a></li>
+                            <li><a href="#">Sub Menu 2</a></li>
+                        </ul>
+                    </li>
+
+                    <li><a><i class="fa fa-clone"></i> Orders <span class="fa fa-chevron-down"></span></a>
+                        <ul class="nav child_menu">
+                            <li><a href="#">Sub Menu 1</a></li>
+                            <li><a href="#">Sub Menu 2</a></li>
+                        </ul>
+                    </li>
+
+                    <li><a><i class="fa fa-clone"></i> Revenue <span class="fa fa-chevron-down"></span></a>
+                        <ul class="nav child_menu">
+                            <li><a href="#">Sub Menu 1</a></li>
+                            <li><a href="#">Sub Menu 2</a></li>
+                        </ul>
+                    </li>
+
                 </ul>
             </div>
             <div class="menu_section">
                 <h3>Live On</h3>
                 <ul class="nav side-menu">
-                    <li><a><i class="fa fa-bug"></i> Additional Pages <span
-                                class="fa fa-chevron-down"></span></a>
-                        <ul class="nav child_menu">
-                            <li><a href="#">Sub Menu 1</a></li>
-                            <li><a href="#">Sub Menu 2</a></li>
-                        </ul>
-                    </li>
-                    <li><a><i class="fa fa-windows"></i> Extras <span class="fa fa-chevron-down"></span></a>
-                        <ul class="nav child_menu">
-                            <li><a href="#">Sub Menu 1</a></li>
-                            <li><a href="#">Sub Menu 2</a></li>
-                        </ul>
-                    </li>
+
+
                     <li><a><i class="fa fa-sitemap"></i> Multilevel Menu <span
                                 class="fa fa-chevron-down"></span></a>
                         <ul class="nav child_menu">
@@ -100,7 +120,9 @@
                             </li>
                         </ul>
                     </li>
-                    <li><a href="javascript:void(0)"><i class="fa fa-laptop"></i> Landing Page <span
+                    <li><a href="javascript:void(0)"><i class="fa fa-laptop"></i> Other
+                            <Features></Features>
+                            <span
                                 class="label label-success pull-right">Coming Soon</span></a></li>
                 </ul>
             </div>

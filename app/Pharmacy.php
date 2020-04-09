3dd1474589a6ed3bd696f8798fc805c16fd98384
Emad Saeed <e@emads3.com>
2020-04-09 12:44:13 +0200

    edit pharmacy model

--- app/Pharmacy.php
+++ app/Pharmacy.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Pharmacy extends Model
 {
-    //
+    protected $fillable = ["name", "email", "password", "national_id", "avatar_image", "priority", "area_id"];
+
+    protected $hidden = ["password"];
+
 }

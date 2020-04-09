f45734766308e50c4ad13b7ae62eaaba9f49b7ff
Emad Saeed <e@emads3.com>
2020-04-09 12:06:11 +0200

    add pharmacy model + migration

---
+++ database/migrations/2020_04_09_075257_create_pharmacies_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePharmaciesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pharmacies', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->string('password');
+            $table->string('national_id')->unique();
+            $table->string('avatar_image');
+            $table->integer('priority');
+            $table->integer('area_id');
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pharmacies');
+    }
+}

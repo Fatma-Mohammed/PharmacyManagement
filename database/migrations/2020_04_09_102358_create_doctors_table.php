3959e27e6dea33ee5313aaa4ad0ed2bf280420a1
Emad Saeed <e@emads3.com>
2020-04-09 12:26:12 +0200

    add Doctor model + migration

---
+++ database/migrations/2020_04_09_102358_create_doctors_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDoctorsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('doctors', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->string('password');
+            $table->string('national_id')->unique();
+            $table->string('avatar_image')->default('default_avatar.jpg');
+            $table->boolean('is_baned')->default(0);
+            $table->unsignedInteger('pharmacy_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('doctors');
+    }
+}

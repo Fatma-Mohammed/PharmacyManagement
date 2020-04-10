1e1a613a790bfc02d878ae9e07692203512f1924
Fatma Mohammed <gfatmamohammed@gmail.com>
2020-04-10 09:40:52 +0200

    make not logged for month notification

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -4,7 +4,7 @@ namespace App\Console;
 
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
-
+use Illuminate\Support\Facades\DB;
 class Kernel extends ConsoleKernel
 {
     /**
@@ -24,8 +24,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')->hourly();
-    }
+        $schedule->command('notify:send')->daily();    }
 
     /**
      * Register the commands for the application.

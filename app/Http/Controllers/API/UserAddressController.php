335cc1be4f1d673d0f4bb94eab4bbe4cdbc19d4d
Fatma Mohammed <gfatmamohammed@gmail.com>
2020-04-10 05:27:28 +0200

    fix add user address

--- app/Http/Controllers/API/UserAddressController.php
+++ app/Http/Controllers/API/UserAddressController.php
@@ -4,7 +4,10 @@ namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use App\UserAddress;
+use App\Address;
+use App\Http\Requests\StoreAddressRequest;
+use Illuminate\Support\Facades\Auth;
+
 
 class UserAddressController extends Controller
 {
@@ -12,7 +15,7 @@ class UserAddressController extends Controller
     {
         
 
-        return  UserAddress::all();
+        return  Address::all();
     }
     public function show()
     {
@@ -20,58 +23,59 @@ class UserAddressController extends Controller
         $addressId = $request->useraddress;
         // $userId=$request->user;
 
-        $useraddress = UserAddress::find($addressId);
+        $useraddress = Address::find($addressId);
     
         return  $useraddress;
 
     }
 
-    public function store(Request $request)
+    public function store(StoreAddressRequest $request)
     {
-        $request = request();
-       return UserAddress::create([
+       return Address::create([
+           
             'area_id' => $request->area_id,
             'street_name' => $request->street_name,
             'building_number' => $request->building_number,
             'floor_number' => $request->floor_number,
-            'falt_number' => $request->falt_number,
+            'flat_number' => $request->flat_number,
             'is_main' => $request->is_main,
+            'user_id' => Auth::id(),
         ]);
        
     }
 
-    public function update()
-    {
-        $request = request();
+    // public function update()
+    // {
+    //     $request = request();
     
-        $useraddress = UserAddress::find($request->useraddress);
-        $useraddress->area_id = $request->area_id;
-        $useraddress->street_name = $request->street_name;
-        $useraddress->building_number = $request->building_number;
-        $useraddress->floor_number = $request->floor_number;
-        $useraddress->falt_number = $request->falt_number;
-        $useraddress->is_main = $request->is_main;
+    //     $useraddress = UserAddress::find($request->useraddress);
+    //     $useraddress->area_id = $request->area_id;
+    //     $useraddress->street_name = $request->street_name;
+    //     $useraddress->building_number = $request->building_number;
+    //     $useraddress->floor_number = $request->floor_number;
+    //     $useraddress->falt_number = $request->falt_number;
+    //     $useraddress->is_main = $request->is_main;
 
-        $useraddress->save();
-        return $useraddress;
+    //     $useraddress->save();
+    //     return $useraddress;
 
-    }
+    // }
 
 
 
-    public function destroy()
-    {
-        $request = request();
-        $addressId = $request->useraddress;
+    // public function destroy()
+    // {
+    //     $request = request();
+    //     $addressId = $request->useraddress;
 
-        // $post = Post::find($postId);
+    //     // $post = Post::find($postId);
 
-        // $post->delete();
-        UserAddress::destroy($addressId);
+    //     // $post->delete();
+    //     UserAddress::destroy($addressId);
 
-        return  UserAddress::all();
+    //     return  UserAddress::all();
 
-    }
+    // }
 
 
 }
--- routes/api.php
+++ routes/api.php
@@ -20,18 +20,15 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 
 Route::post('login', 'API\AuthController@login')->name('login');
 Route::post('register', 'API\AuthController@register')->name('register');
-
-
-Route::get('/useraddresses', 'API\UserAddressController@index')->name('useraddresses.index');
-Route::get('/useraddresses/{useraddress}', 'API\UserAddressController@show')->name('useraddresses.show');
-
-
-Route::post('/useraddresses', 'API\UserAddressController@store')->name('useraddresses.store');
-Route::post('/useraddresses/{useraddress}/update', 'API\UserAddressController@update')->name('useraddresses.update');
-Route::delete('/useraddresses/{useraddress}/destroy', 'API\UserAddressController@destroy')->name('useraddresses.destroy');
 Route::group(['middleware' => ['auth:sanctum', 'verified']], function () {
     Route::get('profile', 'API\UserController@show')->name('user.profile.show');
     Route::put('profile', 'API\UserController@update')->name('user.profile.update');
-    Route::resource('orders', 'API\OrderController')->only(['index','show','store']);
-
+    Route::resource('orders', 'API\OrderController')->only(['index', 'show', 'store']);
+    
+  
+    Route::get('/useraddresses', 'API\UserAddressController@index')->name('useraddresses.index');
+    Route::get('/useraddresses/{useraddress}', 'API\UserAddressController@show')->name('useraddresses.show');
+    Route::post('/useraddresses', 'API\UserAddressController@store')->name('useraddresses.store');
+    Route::post('/useraddresses/{useraddress}/update', 'API\UserAddressController@update')->name('useraddresses.update');
+    Route::delete('/useraddresses/{useraddress}/destroy', 'API\UserAddressController@destroy')->name('useraddresses.destroy');
 });

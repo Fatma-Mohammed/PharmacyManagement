ef2774d3458f255dd609f468444d869b8e12e9f1
Emad Saeed <e@emads3.com>
2020-04-10 15:02:37 +0200

    edit pharmacy model

--- app/Pharmacy.php
+++ app/Pharmacy.php
@@ -8,6 +8,11 @@ class Pharmacy extends Model
 {
     protected $fillable = ["name", "email", "password", "national_id", "avatar_image", "priority", "area_id"];
 
-    protected $hidden = ["password"];
+    protected $hidden = ["password", "deleted_at"];
+
+    public function doctors()
+    {
+        return $this->hasMany('App\Doctor');
+    }
 
 }

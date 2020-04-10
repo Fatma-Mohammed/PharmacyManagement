cec261c6dcda4597e185b5dbe98734958953f816
Emad Saeed <e@emads3.com>
2020-04-10 16:03:20 +0200

    add edit+show views for pharmacies [skeleton only]

---
+++ resources/views/pharmacies/show.blade.php
@@ -0,0 +1,54 @@
+@extends('dashboard-main')
+
+@section('addional_css_includes')
+
+@endsection
+
+@section('content')
+
+    <div class="">
+        <div class="page-title">
+            <div class="title_left">
+                <h3>Test Page</h3>
+            </div>
+        </div>
+
+        <div class="clearfix"></div>
+
+        <div class="row">
+            <div class="col-md-12">
+                <div class="x_panel">
+                    <div class="x_title">
+                        <h2>Title of panel goes here</h2>
+                        <ul class="nav navbar-right panel_toolbox">
+                            <li><a class="collapse-link"><i class="fa fa-chevron-up"></i></a>
+                            </li>
+                            <li class="dropdown">
+                                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button"
+                                   aria-expanded="false"><i class="fa fa-wrench"></i></a>
+                                <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                                    <a class="dropdown-item" href="#">Settings 1</a>
+                                    <a class="dropdown-item" href="#">Settings 2</a>
+                                </div>
+                            </li>
+                            <li><a class="close-link"><i class="fa fa-close"></i></a>
+                            </li>
+                        </ul>
+                        <div class="clearfix"></div>
+                    </div>
+                    <div class="x_content">
+                        <section>
+                            <h1>Show single pharmacy details</h1>
+                        </section>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+@endsection
+
+
+@section('addional_js_includes')
+
+@endsection

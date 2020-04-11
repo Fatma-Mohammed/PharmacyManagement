abb0b314afa2f2c2cd277c3c80d50d7f52972a48
Emad Saeed <e@emads3.com>
2020-04-11 17:29:17 +0200

    show single pharmacy view

--- resources/views/pharmacies/show.blade.php
+++ resources/views/pharmacies/show.blade.php
@@ -9,7 +9,7 @@
     <div class="">
         <div class="page-title">
             <div class="title_left">
-                <h3>Test Page</h3>
+                <h3>{{ $pharmacy->name }}</h3>
             </div>
         </div>
 
@@ -19,7 +19,7 @@
             <div class="col-md-12">
                 <div class="x_panel">
                     <div class="x_title">
-                        <h2>Title of panel goes here</h2>
+                        <h2>Info of Pharmacy #{{ $pharmacy->id }}</h2>
                         <ul class="nav navbar-right panel_toolbox">
                             <li><a class="collapse-link"><i class="fa fa-chevron-up"></i></a>
                             </li>
@@ -37,9 +37,83 @@
                         <div class="clearfix"></div>
                     </div>
                     <div class="x_content">
-                        <section>
-                            <h1>Show single pharmacy details</h1>
-                        </section>
+
+                        <form class="form-horizontal form-label-left">
+                            <div class="text-center">
+                                <img src="/storage/{{$pharmacy->avatar_image}}" class="img-fluid rounded"
+                                     alt="Responsive image">
+                            </div>
+                            <br>
+                            <div class="form-group row">
+                                <label class="control-label col-md-3 col-sm-3 ">Pharmacy ID</label>
+                                <div class="col-md-9 col-sm-9 ">
+                                    <input type="text" class="form-control" readonly="readonly"
+                                           placeholder="{{ $pharmacy->id }}">
+                                </div>
+                            </div>
+                            <div class="form-group row">
+                                <label class="control-label col-md-3 col-sm-3 ">Pharmacy Name</label>
+                                <div class="col-md-9 col-sm-9 ">
+                                    <input type="text" class="form-control" readonly="readonly"
+                                           placeholder="{{ $pharmacy->name }}">
+                                </div>
+                            </div>
+                            <div class="form-group row">
+                                <label class="control-label col-md-3 col-sm-3 ">Pharmacy Email</label>
+                                <div class="col-md-9 col-sm-9 ">
+                                    <input type="text" class="form-control" readonly="readonly"
+                                           placeholder="{{ $pharmacy->email }}">
+                                </div>
+                            </div>
+                            <div class="form-group row">
+                                <label class="control-label col-md-3 col-sm-3 ">Pharmacy National ID</label>
+                                <div class="col-md-9 col-sm-9 ">
+                                    <input type="text" class="form-control" readonly="readonly"
+                                           placeholder="{{ $pharmacy->national_id }}">
+                                </div>
+                            </div>
+                            <div class="form-group row">
+                                <label class="control-label col-md-3 col-sm-3 ">Pharmacy Priority</label>
+                                <div class="col-md-9 col-sm-9 ">
+                                    <input type="text" class="form-control" readonly="readonly"
+                                           placeholder="{{ $pharmacy->priority }}">
+                                </div>
+                            </div>
+                            <div class="form-group row">
+                                <label class="control-label col-md-3 col-sm-3 ">Pharmacy Area ID</label>
+                                <div class="col-md-9 col-sm-9 ">
+                                    <input type="text" class="form-control" readonly="readonly"
+                                           placeholder="{{ $pharmacy->area_id }}">
+                                </div>
+                            </div>
+                            <div class="form-group row">
+                                <label class="control-label col-md-3 col-sm-3 ">Pharmacy Creation Date</label>
+                                <div class="col-md-9 col-sm-9 ">
+                                    <input type="text" class="form-control" readonly="readonly"
+                                           placeholder="{{ $pharmacy->created_at }}">
+                                </div>
+                            </div>
+                            <div class="form-group row">
+                                <label class="control-label col-md-3 col-sm-3 ">Last Modification On: </label>
+                                <div class="col-md-9 col-sm-9 ">
+                                    <input type="text" class="form-control" readonly="readonly"
+                                           placeholder="{{ $pharmacy->updated_at }}">
+                                </div>
+                            </div>
+
+                            <div class="ln_solid"></div>
+                            <div class="form-group">
+                                <div class="col-md-9 col-sm-9  offset-md-3">
+                                    <a href="{{ route('pharmacies.destroy') }}" class="btn btn-danger">Delete</a>
+                                    <a href="{{ route('pharmacies.edit', $pharmacy->id) }}"
+                                       class="btn btn-primary">Edit</a>
+                                    <a href="{{ route('pharmacies.index') }}" class="btn btn-success">All Pharmacies
+                                        Page</a>
+                                </div>
+                            </div>
+
+                        </form>
+
                     </div>
                 </div>
             </div>
